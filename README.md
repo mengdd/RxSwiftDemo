8249eaa80ab9a1e59ff44046915a2255f8e7b1da
Dandan Meng <mengdd@users.noreply.github.com>
2023-01-06 09:29:52 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# iOSHelloWorldTemplate
+A template project to generate new project immediately.
+
+## Tasks
+### Infrastructures
+* [] a good .gitignore
+* [] github actions: run checks
+* [] swiftformat rules
+* [] hooks to run swift lint
+
+### Code
+* [x] all code, no storyboard
+
+### Test
+* [] typical Unit Test example
---
+++ iOSHelloWorldTemplate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSHelloWorldTemplate
+//
+//  Created by Dandan Meng on 2022/12/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = .systemBlue
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
